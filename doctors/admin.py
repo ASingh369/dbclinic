114d81e452a0545b1c87713db8ef7eefd1f2d70f
Amrit Pal Singh <amritpalsinghs369@gmail.com>
2019-04-07 01:14:26 -0700

    customized admin area

--- doctors/admin.py
+++ doctors/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 
 from .models import Doctor, Schedule, Appointment
 
-admin.site.register(Doctor)
-admin.site.register(Appointment)
-admin.site.register(Schedule)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'docName', 'docType', 'email')
+    list_display_links = ('id', 'docName')
+    list_filter = ('docType',)
+    search_fields = ('docName', 'docType', 'email')
+    list_per_page = 10
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'schedule')
+    list_display_links = ('id', 'user')
+    search_fields = ('id', 'user')
+    list_per_page = 10
+
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'doctor', 'appointment_date', 'time', 'booked')
+    list_display_links = ('id', 'doctor')
+    list_filter = ('booked', 'doctor', 'appointment_date')
+    search_fields = ('doctor', 'appointment_date')
+    list_per_page = 20
+
+admin.site.register(Doctor, DoctorAdmin)
+admin.site.register(Appointment, AppointmentAdmin)
+admin.site.register(Schedule, ScheduleAdmin)
--- pharmacy/admin.py
+++ pharmacy/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 
 from .models import Medicine, Order, Contact
 
-admin.site.register(Medicine)
-admin.site.register(Order)
-admin.site.register(Contact)
+class MedicineAdmin(admin.ModelAdmin):
+    list_display = ('id', 'medName', 'medType', 'medCost', 'medQuantity', 'medDate')
+    list_display_links = ('id', 'medName')
+    list_filter = ('medType',)
+    search_fields = ('medName', 'medInfo')
+    list_per_page = 10
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'medicine', 'name', 'email', 'quantity', 'order_date')
+    list_display_links = ('id', 'medicine')
+    list_filter = ('quantity', )
+    search_fields = ('medicine', 'name', 'email', 'quantity')
+    list_per_page = 10
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'contact_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'message', 'email')
+    list_per_page = 10
+
+admin.site.register(Medicine, MedicineAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Contact, ContactAdmin)
